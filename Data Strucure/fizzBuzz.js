c21221ef25db536f7989b16b520f7a2df69717b9
RH saimum <rhsaimum@RHs-MacBook-Pro.local>
2025-04-07 15:32:02 +0600

    added data structures lessons

---
+++ Data Strucure/fizzBuzz.js
@@ -0,0 +1,18 @@
+const fizzBuzz = (n) => {
+    const result =[]
+  
+    for (let i =1;i<=n ;i++){
+      if(i%3=== 0 && i%5 === 0){
+      result.push("fizzbuzz")
+      }else if(i%3=== 0){
+        result.push("fizz")
+      }else if(i%5=== 0){
+        result.push("buzz")
+      }else{
+        result.push(i)
+      }
+   }return result
+    
+  };
+
+  console.log("fizzBuzz :",fizzBuzz(13))
