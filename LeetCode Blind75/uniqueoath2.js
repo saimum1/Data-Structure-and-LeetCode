0baad35c7da2d5342004ccc6bea31be309e409db
RH saimum <rhsaimum@RHs-MacBook-Pro.local>
2025-05-02 19:33:51 +0600

    new

---
+++ LeetCode Blind75/uniqueoath2.js
@@ -0,0 +1,14 @@
+
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    return countPaths(obstacleGrid)
+};
+
+const countPaths = (grid, r=0, c=0, memo={}) => {
+    const pos = r + ',' + c
+    if(pos in memo) return memo[pos]
+    if(r === grid.length || c === grid[0].length || grid[r][c] === 1) return 0
+    if(r === grid.length - 1 && c === grid[0].length - 1) return 1
+    memo[pos]= countPaths(grid, r+1, c, memo) + countPaths(grid, r, c+1, memo)
+
+    return memo[pos]
+};
